7c1dc73ce85a52c947946a74880a9a363506a651
shwetamisar <62926371+shwetamisar@users.noreply.github.com>
2022-12-06 13:50:15 +0530

    update : updated css file

--- app.js
+++ app.js
@@ -1,9 +1,33 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
-var outputDiv = document.querySelector("#output");
+//button click
+var btnTranslate=document.querySelector("#btn-translate");
+//take input from text area
+var textareaInput=document.querySelector(".textarea-input");
+//show op
+var outputArea=document.querySelector(".output-area");
 
+//var serverURL="https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
 
-function clickHandler() {
-    outputDiv.innerText = "kjhdghbisfhcvsdhgv"+ txtInput.value;
-} 
-btnTranslate.addEventListener("click", clickHandler)
+var serverURL="https://api.funtranslations.com/translate/minion.json"
+function getTranslationURL(textareaInput)
+{
+    return serverURL+"?"+"text="+textareaInput;
+}
+function errorHandler(error)
+{
+    console.log("error occured",error)
+    alert("Sever issue !! Try after sometime")
+}
+function clickEventHandler()
+{
+   //outputArea.innerText="gdhf"+textareaInput.value;
+   var inputText=textareaInput.value;
+   fetch(getTranslationURL(inputText))
+   .then(response => response.json())
+   .then(json =>{
+       var translatedText=json.contents.translated;
+       outputArea.innerText=translatedText;
+   })
+   .catch(errorHandler)
+
+};
+btnTranslate.addEventListener("click",clickEventHandler)
